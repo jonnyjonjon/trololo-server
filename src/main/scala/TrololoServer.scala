f2d56661378237baeb23a0b2d098d8f9cd98ce4a
Jonathan Morabito <jonathanmorabito@spotify.com>
2017-06-25 13:50:28 -0400

    attempts to validate ip against open source ip api

--- src/main/scala/TrololoServer.scala
+++ src/main/scala/TrololoServer.scala
@@ -1,20 +1,68 @@
 import akka.actor.ActorSystem
-import akka.event.{LoggingAdapter, Logging}
+import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.{ActorMaterializer, Materializer}
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
-import scala.concurrent.ExecutionContextExecutor
+import spray.json.DefaultJsonProtocol
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+case class IpInfo(query: String,
+                  status: String,
+                  country: Option[String],
+                  message: Option[String])
+
+trait Protocols extends DefaultJsonProtocol {
+  implicit val ipInfoFormat = jsonFormat4(IpInfo.apply)
+}
 
 trait Service {
   implicit val system: ActorSystem
+
   implicit def executor: ExecutionContextExecutor
+
   implicit val materializer: Materializer
 
   def config: Config
+
   val logger: LoggingAdapter
+
+  lazy val ipApiConnectionFlow: Flow[HttpRequest, HttpResponse, Any] = Http().outgoingConnection(
+    config.getString("services.ip-api.host"),
+    config.getInt("services.ip-api.port"))
+
+  def ipApiRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request)
+    .via(ipApiConnectionFlow).runWith(Sink.head)
+
+  def fetchIpInfoMessage(ip: String): Future[String] = {
+    ipApiRequest(RequestBuilding.Get(s"/json/$ip")).flatMap { response =>
+      response.status match {
+        case OK =>
+          Unmarshal(response.entity).to[IpInfo].map { ipInfo =>
+            (ipInfo.status, ipInfo.country, ipInfo.message) match {
+              case ("success", Some(country), None) =>
+                s"success for query ${ipInfo.query}: $country"
+              case ("fail", None, Some(message)) =>
+                s"failure for query ${ipInfo.query}: $message"
+              case _ =>
+                s"failure for query ${ipInfo.query}: returned invalid ip info $ipInfo"
+            }
+          }
+        case _ =>
+          Unmarshal(response.entity).to[String].map { entity =>
+            s"failure for query $ip: bad status code ${response.status}, returned entity $entity"
+          }
+      }
+    }
+  }
 
   val routes = {
     logRequestResult("trololo-server") {
@@ -22,21 +70,16 @@ trait Service {
         get {
           (parameters('ip.?) & optionalHeaderValueByName("X-Forwarded-For")) { (maybeIp, maybeXff) =>
             complete {
-              val ipMessage = maybeIp match {
-                case Some(ip) =>
-                  s"ip parameter value:$ip, "
-                case _ =>
-                  s"ip parameter missing! "
-              }
-              val xffMessage = maybeXff match {
+              maybeXff match {
                 case Some(xff) =>
-                  s"xff header value:$xff"
+                  fetchIpInfoMessage(xff).map[ToResponseMarshallable] { message =>
+                    logger.error(message)
+                    NoContent
+                  }
                 case _ =>
-                  s"xff header missing!"
+                  logger.error("failure: no x-forwarded-for header to parse")
+                  NoContent
               }
-              logger.error(ipMessage + xffMessage)
-              // always completing with a 204 to trigger passback
-              NoContent
             }
           }
         }
