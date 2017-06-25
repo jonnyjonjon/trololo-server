82cca1de23f053c041a0f2c375fa7f12af505851
Jonathan Morabito <jonathanmorabito@spotify.com>
2017-06-25 14:12:06 -0400

    fixed compile

--- src/main/scala/TrololoServer.scala
+++ src/main/scala/TrololoServer.scala
@@ -1,14 +1,15 @@
 import akka.actor.ActorSystem
-import akka.event.{Logging, LoggingAdapter}
+import akka.event.{LoggingAdapter, Logging}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import spray.json.DefaultJsonProtocol
@@ -24,7 +25,7 @@ trait Protocols extends DefaultJsonProtocol {
   implicit val ipInfoFormat = jsonFormat4(IpInfo.apply)
 }
 
-trait Service {
+trait Service extends Protocols {
   implicit val system: ActorSystem
 
   implicit def executor: ExecutionContextExecutor
